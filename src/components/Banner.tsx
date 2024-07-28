32a3c2dd1d98d41bce1f506be4fb0a54b2ab28bf
axelobscura <axosar@gmail.com>
2024-07-28 07:16:44 -0600

    imcycgtp

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image';
+
 export default function Banner() {
-  return <span className="sm:text-5xl text-3xl pt-4">ASISTENTE IMCYC CONCRETON</span>;
+  return <span className="sm:text-5xl text-3xl pt-4">
+    <Image
+          src="/logo.svg"
+          width={250}
+          height={45}
+          alt="Acervo del cemento y del concreto - Instituto Mexicano del Cemento y del Concreto A.C."
+          style={{
+            display: 'block',
+            margin: '0 auto',
+          }}
+        />
+  </span>;
 }
--- src/pages/api/chatgpt.ts
+++ src/pages/api/chatgpt.ts
@@ -10,9 +10,22 @@ export default async function handler(
 
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: chatHistory,
+    messages: [
+      {
+        role: 'system',
+        content: `Este GPT actúa como un asistente experto en el Instituto Mexicano del Cemento y del Concreto (IMCYC)(https://www.imcyc.com/), especializado en proporcionar información sobre los archivos PDF disponibles en la Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/) y de la página web https://www.imcyc.com/.  Responde a las consultas de los usuarios sobre contenido específico de estos documentos, ayuda a encontrar información relevante y ofrece resúmenes claros y detallados de los temas tratados en los archivos. Además, guía a los usuarios sobre cómo acceder y utilizar la Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/) y de la página web https://www.imcyc.com/ de manera efectiva.
+
+        Cada vez que se mencione Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/), lo presentará como un enlace.
+
+        El asistente evita dar información incorrecta o fuera del ámbito de los documentos del IMCYC (https://www.imcyc.com/). También puede crear resúmenes. Todas las respuestas se basarán en la información disponible en Biblioteca IMCYC (https://imcyc.com/ArchivosPDF/). Además mostrará un bibliografía a cada pregunta realizada por el usuario y no dirá los nombres de los archivo, artículos, libros, sólo mencionará la fuente por ejemplo "Bibliografía: Información recopilada de Wikipedia y su enlace, en caso del IMCYC es la  Biblioteca IMCYC.`,
+      },
+      {
+        role: 'user',
+        content: `toda la información relacionada sobre el siguiente tema: ${chatHistory}`,
+      },
+    ],
     temperature: 1,
-    max_tokens: 200,
+    max_tokens: 2000,
   });
 
   const chatGptResponse = completion.data.choices[0].message?.content;
