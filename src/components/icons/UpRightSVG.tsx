13f46444182489777cd565b92535857b97eb5212
axelinstrumental <aobscura@instrumental.net>
2024-08-14 18:15:40 -0600

    boton

--- src/components/icons/UpRightSVG.tsx
+++ src/components/icons/UpRightSVG.tsx
@@ -9,7 +9,7 @@ export default function UpRightSVG() {
     >
       <path
         d="M10.5253 5.49475L10.5206 7.49475L15.0782 7.50541L5.47473 17.0896L6.88752 18.5052L16.5173 8.89479L16.5065 13.5088L18.5065 13.5134L18.5253 5.51345L10.5253 5.49475Z"
-        fill="currentColor"
+        fill="#ffffff"
       />
     </svg>
   );
