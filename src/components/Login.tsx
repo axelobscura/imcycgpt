725583fd07cfec2d69c8472e64eb223efcaad1ef
axelinstrumental <aobscura@instrumental.net>
2024-12-18 10:38:14 -0600

    login

---
+++ src/components/Login.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+
+export default function Login() {
+  const loginTest = () => {
+    const username = (document.getElementById('username') as HTMLInputElement).value;
+    const password = (document.getElementById('password') as HTMLInputElement).value;
+    if (username === 'admin' && password === 'admin') {
+      alert('Bienvenido');
+    } else {
+      alert('Usuario o contraseña incorrectos');
+    }
+  };
+  return (
+    <div className="max-h-screen flex items-center justify-center flex-col sm:text-5xl text-3xl w-full bg-slate-800">
+      <Image
+        src="/concreton.png"
+        width={250}
+        height={45}
+        alt="Acervo del cemento y del concreto - Instituto Mexicano del Cemento y del Concreto A.C."
+        style={{
+          display: 'block',
+          margin: '0 auto',
+        }}
+      />
+      <h2 className='text-center font-light text-gray-500 text-2xl mt-3'>ASISTENTE CONCRETON IMCYC</h2>
+      <p className='text-gray-300 text-sm text-center font-light'>Asistente en tecnología del Concreto y del Cemento</p>
+      <div className='mt-3'>
+        <form onSubmit={loginTest} className="bg-white shadow-md rounded px-10 pt-6 pb-8 mb-2">
+          <div className="mb-3">
+            <label className="block text-gray-700 text-sm font-bold mb-0 pb-0">
+              Usuario:
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-base" id="username" type="text"/>
+          </div>
+          <div className="mb-3">
+            <label className="block text-gray-700 text-sm font-bold mb-0">
+              Contraseña:
+            </label>
+            <input className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline text-base" id="password" type="password"/>
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="bg-slate-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline w-full text-sm" type="submit">
+              INGRESAR
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import App from "@/components/App";
+import Login from "@/components/Login";
 
 export default function Home() {
-  return <App />;
+  return <Login />;
 }
